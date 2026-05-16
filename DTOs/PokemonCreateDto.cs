e35b77250265b4ab923cbde9a0255e00bf4f53ee
erick190813 <erickaraujo1908@gmail.com>
2026-05-16 17:31:45 -0300

    Adicionando DTO's

--- DTOs/PokemonCreateDto.cs
+++ DTOs/PokemonCreateDto.cs
@@ -1,6 +1,18 @@
-﻿namespace APIMonsterDeConexão.DTOs
+﻿namespace ApiMonsterDeConexao.DTOs
 {
     public class PokemonCreateDto
     {
+        public double Altura { get; set; }
+        public double Peso { get; set; }
+        public List<string> Tipos { get; set; } = new List<string>();
+        public string SpriteUrl { get; set; } = string.Empty;
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int SpAttack { get; set; }
+        public int SpDefense { get; set; }
+        public int Speed { get; set; }
+        public int BaseStatTotal { get; set; }
+        public string CompetitiveRole { get; set; } = string.Empty;
     }
 }
--- DTOs/PokemonResponseDto.cs
+++ DTOs/PokemonResponseDto.cs
@@ -1,6 +1,9 @@
-﻿namespace APIMonsterDeConexão.DTOs
+﻿namespace ApiMonsterDeConexao.DTOs
 {
-    public class PokemonResponseDto
+    public class PokemonResponseDto : PokemonCreateDto
     {
+        public string Id { get; set; } = string.Empty;
+        public DateTime DataColeta { get; set; }
+        public bool EnviadoParaNuvem { get; set; }
     }
 }
